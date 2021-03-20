b636f3c1c36ac13ce28299ea9012a95134758201
stephanielichtsteiner <77996666+stephanielichtsteiner@users.noreply.github.com>
2021-03-20 21:12:22 +0100

    Added forecast elements

--- src/index.js
+++ src/index.js
@@ -8,9 +8,22 @@ let minutes = date.getMinutes();
 if (minutes < 10){
     minutes = `0${minutes}`;
 }
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
+}
+
+function formatHours(timestamp){
+    let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10){
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10){
+    minutes = `0${minutes}`;
+}
+    return `${hours}:${minutes}`;
 }
 
 function displayTemperature(response){
@@ -22,7 +35,10 @@ let windSpeed = document.querySelector("#wind-speed");
 let humidity = document.querySelector("#humidity");
 let dateElement = document.querySelector("#date");
 let iconElement = document.querySelector("#icon");
-temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+celsiusTemperature = response.data.main.temp;
+
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
 cityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 feltTemperature.innerHTML = Math.round(response.data.main.feels_like);
@@ -33,11 +49,32 @@ iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.dat
 iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function search (city){
+function displayForecast(response){
+    let forecastElement = document.querySelector("#forecast");
+    let forecast = response.data.list[0];
+
+    forecastElement.innerHTML = `
+    <div class="col-2">
+    <div class="card saturday " style="">
+    <h3>${formatHours(forecast.dt * 1000)}</h3>
+        <div class="card-body">
+        <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"/>
+            <p class="card-text"> ${Math.round(forecast.main.temp_max)}°C | ${Math.round(forecast.main.temp_min)}°C</p>
+        </div>
+    </div>
+</div>
+    `;
+
+}
+
+function search(city){
 let apikey = "c3e2e398dc000b29ea3b92e856aeecfa";
 let apiurl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
 
 axios.get(apiurl).then(displayTemperature);
+
+apiurl =`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}&units=metric`;
+axios.get(apiurl).then(displayForecast);
 }
 
 function handleSubmit(event){
@@ -46,9 +83,31 @@ function handleSubmit(event){
 search(cityNameElement.value);
 }
 
-search("Berlin");
 
+function displayFahrenheitTemperature(event){
+    event.preventDefault();
+    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event){
+    event.preventDefault();
+        let temperatureElement = document.querySelector("#temperature");
+        temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 
 let form = document.querySelector("#search");
 form.addEventListener("submit", handleSubmit);
+
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Berlin");

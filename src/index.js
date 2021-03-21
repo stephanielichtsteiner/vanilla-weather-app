e7ed03996052eb803582d045ea104a933fd8e5ba
stephanielichtsteiner <77996666+stephanielichtsteiner@users.noreply.github.com>
2021-03-21 19:53:41 +0100

    Finished unit conversion

--- src/index.js
+++ src/index.js
@@ -84,12 +84,16 @@ search(cityNameElement.value);
 function displayFahrenheitTemperature(event){
     event.preventDefault();
     let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
 function displayCelsiusTemperature(event){
     event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
         let temperatureElement = document.querySelector("#temperature");
         temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
